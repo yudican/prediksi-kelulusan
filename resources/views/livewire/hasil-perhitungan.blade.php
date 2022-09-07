f56c9cd421db96e3fc68cb5724181a1a7093d9b2
Yudi Candra <yudhichandra02@gmail.com>
2022-09-07 21:01:32 +0700

    update color

--- resources/views/livewire/hasil-perhitungan.blade.php
+++ resources/views/livewire/hasil-perhitungan.blade.php
@@ -137,33 +137,29 @@
             function loadChartBar(dataChartBar=[]) {
                 console.log(dataChartBar,'dataChartBar')
                 var barChart = document.getElementById('mhs-chart').getContext('2d')
-                const datac = {
-                labels: ['label1', 'label2', 'label3', 'label4', 'label5', 'label6', 'label7'],
-                datasets: [{
-                    label: 'My First Dataset',
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 159, 64, 0.2)',
-                        'rgba(255, 205, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(201, 203, 207, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(255, 159, 64)',
-                        'rgb(255, 205, 86)',
-                        'rgb(75, 192, 192)',
-                        'rgb(54, 162, 235)',
-                        'rgb(153, 102, 255)',
-                        'rgb(201, 203, 207)'
-                    ],
-                    borderWidth: 1
-                }]
-                };
-                var myBarChart = new Chart(barChart, datac)
+                var myBarChart = new Chart(barChart, {
+                    type: 'bar',
+                    data: {
+                        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                        datasets : [{
+                            label: "Sales",
+                            backgroundColor: 'rgb(23, 125, 255)',
+                            borderColor: 'rgb(23, 125, 255)',
+                            data: [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4],
+                        }],
+                    },
+                    options: {
+                        responsive: true, 
+                        maintainAspectRatio: false,
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero:true
+                                }
+                            }]
+                        },
+                    }
+                });
             }
             const dataChart = @json($dataChart);
             loadChart(dataChart);
